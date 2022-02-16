fb6400f8b1e2e122ee2be1ad70b38e302d5ffd1a
John Clausel <jcclausel@gmail.com>
2022-02-16 13:02:32 -0500

    [dev] display skills and section (HTML, CSS, JS)

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,30 @@ function scrollHeader(){
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', scrollHeader);
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.service-modal'),
+      modalBtns = document.querySelectorAll('.service-btn'),
+      modalClose = document.querySelectorAll('.service-modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+    console.log("allo")
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
