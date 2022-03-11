ba74a123349f04341fb50c34c74273630581f235
John Clausel <jcclausel@gmail.com>
2022-03-11 10:19:02 -0500

    Interface site web set wit the differents breakpoints

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,6 @@ let modal = function(modalClick){
 }
 
 modalBtns.forEach((mb, i) => {
-    console.log("allo")
     mb.addEventListener('click', () => {
         modal(i)
     })
@@ -29,20 +28,91 @@ modalClose.forEach((mc) => {
     })
 })
 
-/*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/ 
+let mixerPortfolioFilter = mixitup('.work-container', {
+    selectors: {
+        target: '.work-card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /* Link active work */ 
+const workFilterLink = document.querySelectorAll('.work-filter-item')
 
+function activeWorkFilter() {
+    workFilterLink.forEach(L => L.classList.remove('work-filter-item-active'))
+    this.classList.add('work-filter-item-active')
+}
+workFilterLink.forEach(L=> L.addEventListener('click', activeWorkFilter))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-
+let swiperTestimonial = new Swiper('.testimonial-container', {
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+        },
+    },
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
+const sectionAll = document.querySelectorAll('section[id]')
+function scrollActive() {
+    const scrollY = window.pageYOffset
+    sectionAll.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+        
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== LIGHT DARK THEME ===============*/ 
+const themeButton = document.getElementById('change-theme')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIconTheme = localStorage.getItem('selected-icon')
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIconTheme = () => themeButton.classList.contains(iconTheme) ? 'bx bx-monn' : 'bx bx-sun'
 
+if(selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedTheme === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
 
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme)
+    localStorage.setItem('selected-icon', getCurrentIcon)
+})
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
+const screv = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true,
+})
+screv.reveal(`.home-data`)
+screv.reveal(`.home-img-handle`, {delay: 700})
+screv.reveal(`.home-social, .home-social-2, .home-scroll`, {delay: 700, origin: 'bottom'})
